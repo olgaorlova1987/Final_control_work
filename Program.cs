846d5e55222c6b004592fd4d556d18424540db90
Olga <olga.orlova1987@gmail.com>
2023-07-27 21:17:35 +0300

    Работает, но есть небольшая ошибка, будем устранять

--- Program.cs
+++ Program.cs
@@ -12,34 +12,39 @@
 
 
 Console.WriteLine("Введите количество элементов массива: ");
-int size = int.Parse(Console.ReadLine());
+int size = Convert.ToInt32(Console.ReadLine());
 string[] arr1 = new string[size];
 for (int i = 0; i < size; i++)
 {
-	Console.Write("Введите элемент массива: ");
-    string result = Console.ReadLine();
-	arr1[i] = result;  
+    Console.Write("Введите элемент массива: ");
+    arr1[i] = Console.ReadLine();
 }
+
 Console.WriteLine();
 Console.WriteLine($"Введенный массив: [{string.Join(",", arr1)}]");
-
 int count = 0;
 int maxSymbols = 3;
 
-for (int i = 0; i < arr1.Length; i++){
-	if(arr1[i].Length <= maxSymbols){
-		count++;
-	}   
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= maxSymbols)
+    {
+        count++;
+    }
 }
 Console.WriteLine();
 
+Console.Write("Полученный массив: [");
 string[] arr2 = new string[count];
 int j = 0;
-for (int i = 0; i < arr1.Length; i++){
-    if(arr1[i].Length <= maxSymbols){
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= maxSymbols)
+    {
         arr2[j] = arr1[i];
         Console.Write(arr2[j] + ",");
         j++;
     }
 }
-Console.WriteLine($"Полученный массив: {arr2}");
+
+Console.Write("]");
